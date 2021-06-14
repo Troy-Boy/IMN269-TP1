48da0b0092b0d3c1fbca021c16f0d4dab33b6437
lana2914 <achille.lanctot-saumure@usherbrooke.ca>
2021-06-14 15:04:49 -0400

    git ignore

---
+++ main.py
@@ -0,0 +1,26 @@
+import numpy as np
+import PIL
+from PIL import Image
+
+# print('Pillow Version:', PIL.__version__)
+
+# load and show an image with Pillow
+# Open the image form working directory
+image = Image.open('Koala_climbing_tree.jpg')
+# summarize some details about the image
+print(image.format)
+print(image.size)
+print(image.mode)
+
+# show the image
+image.show()
+
+# Two matrices are
+#  initialized by value
+x = np.array([[1, 2], [4, 5]])
+y = np.array([[7, 8], [9, 10]])
+
+
+#  add()is used to add matrices
+print("Addition of two matrices: ")
+print(np.add(x, y))
