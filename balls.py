d8a60f1a6dfe65b406bd59192fdf1e3e60c52999
lana2914 <achille.lanctot-saumure@usherbrooke.ca>
2021-06-17 12:35:38 -0400

    ca avance. pas vite mais ca avance.

--- balls.py
+++ balls.py
@@ -1,19 +1,22 @@
+import numpy as np
 print("Hello Dork")
 
 # from stack overflow
 # https://stackoverflow.com/questions/3252194/numpy-and-line-intersections
-from numpy import *
-def perp( a ) :
-    b = empty_like(a)
+
+
+def perp(a):
+    b = np.empty_like(a)
     b[0] = -a[1]
     b[1] = a[0]
     return b
 
-def seg_intersect(a1,a2, b1,b2) :
+
+def seg_intersect(a1, a2, b1, b2):
     da = a2-a1
     db = b2-b1
     dp = a1-b1
     dap = perp(da)
-    denom = dot( dap, db)
-    num = dot( dap, dp )
+    denom = np.dot(dap, db)
+    num = np.dot(dap, dp)
     return (num / denom.astype(float))*db + b1
--- imn_utils.py
+++ imn_utils.py
@@ -8,51 +8,70 @@ import balls
 
 # Picture path
 img = cv2.imread('images/church.jpg')
-coordoX = []
-coordoY = []
-f1 = []
-f2 = []
-horizon = []
+coordoX = [556, 759, 563, 763, 553, 226, 414, 346]
+coordoY = [746, 687, 535, 582, 747, 721, 255, 273]
+# horizon = [0, 0, 0]
 
 
 def update(event, x, y, flags, param):
-    if (event == cv2.EVENT_LBUTTONDOWN):
-        xy = "%d,%d" % (x, y)
-        coordoX.append(x)
-        coordoY.append(y)
-        print(x, y)
+    # if (event == cv2.EVENT_LBUTTONDOWN):
+    #     xy = "%d,%d" % (x, y)
+    #     # coordoX.append(x)
+    #     # coordoY.append(y)
+    #     print(x, y)
 
-        if(len(coordoX) % 2 == 0):
-            print("should draw line")
-            cv2.line(img, (x, y),
-                     (coordoX[len(coordoX)-2], coordoY[len(coordoX)-2]),
+    for i in coordoX:
+        if(i % 2 != 0):
+
+            cv2.line(img, (coordoX[i], coordoY[i]),
+                     (coordoX[i+1], coordoY[i+1]),
                      (0, 0, 0), thickness=3)
-        cv2.circle(img, (x, y), 1, (200, 200, 200), thickness=-1)
-        cv2.putText(img, xy, (x, y), cv2.FONT_HERSHEY_PLAIN,
+
+        cv2.circle(img, (coordoX[i],  coordoY[i]),
+                   1, (200, 200, 200), thickness=-1)
+        cv2.putText(img, (coordoX[i],  coordoY[i]), cv2.FONT_HERSHEY_PLAIN,
                     1.0, (0, 0, 0), thickness=1)
-        cv2.imshow("fenetre image", img)
+
+    cv2.imshow("fenetre image", img)
 
     if (len(coordoX) == 8 and len(coordoY) == 8):
         f1 = calculIntersection(0)  # marche pas mais bon who cares amarite
         f2 = calculIntersection(4)
-        horizon = calculateHorizon()
+        horizon = calculateHorizon(f1, f2)
+        print("thats horizon:")
         print(horizon)
-        cv2.line(img, (horizon[0] * 100, horizon[1] * 100), (horizon[0] * 100 + 100, horizon[1] * 100 + 100)
-                 (255, 0, 0), thickness=3)
-        cv2.imshow("fenetre image", img)
+        matH = calculateH(horizon)
+        imgTransfo = np.array((len(img[0]), len(img)))
+        imgTransfo = cv2.warpPerspective(img, matH, (len(img[0]), len(
+            img)), flags=cv2.WARP_INVERSE_MAP, borderMode=cv2.BORDER_CONSTANT)
+
+        cv2.imshow("Resultat", imgTransfo)
 
 
-def calculateHorizon():
+def calculateH(df):
+    h2 = np.zeros((3, 3), dtype=np.float64)
+    h2[0, 0] = 1
+    h2[1, 1] = 1
+    h2[2, :] = df
+    print("hold the line, TA TA TA TA, love isn't always on time\n")
+    print(h2)
+    return h2
+
+
+def calculateHorizon(f1, f2):
     # pFuite1 = balls.seg_intersect(coordoX[0]-coordoX[1], coordoY[0] - coordoY[1],
     #                               coordoX[2]-coordoX[3], coordoY[2] - coordoY[3])
     # pFuite2 = balls.seg_intersect(coordoX[4]-coordoX[5], coordoY[4] - coordoY[5],
     #                               coordoX[6]-coordoX[7], coordoY[6] - coordoY[7])
     # return np.matmul(pFuite1, pFuite2)
 
+    print("thats f1:")
+    print(f1)
+    print("thats f2:")
+    print(f2)
+
     d_horizon = np.cross(f1, f2)
 
-    print(f1)
-    print(f2)
     return d_horizon
 
 
@@ -61,23 +80,35 @@ def calculIntersection(index):
     y1 = coordoY[index] / 100
     p1 = [x1, y1, 1]
 
+    print("thats p1:", p1)
+
     x2 = coordoX[index+1] / 100
     y2 = coordoY[index+1] / 100
     p2 = [x2, y2, 1]
 
+    print("thats p2:", p2)
+
     d1 = np.cross(p1, p2)
+
+    print("thats d1:", d1)
 
     x3 = coordoX[index+2] / 100
     y3 = coordoY[index+2] / 100
     p3 = [x3, y3, 1]
 
+    print("thats p3:", p3)
+
     x4 = coordoX[index+3] / 100
     y4 = coordoY[index+3] / 100
     p4 = [x4, y4, 1]
 
+    print("thats p4:", p4)
+
     d2 = np.cross(p3, p4)
+    print("thats d2:", d2)
 
     f = np.cross(d1, d2)
+    print("thats f:", f)
 
     return f
 
@@ -86,4 +117,5 @@ cv2.namedWindow("fenetre image")
 cv2.setMouseCallback("fenetre image", update)
 cv2.imshow("fenetre image", img)
 cv2.waitKey(0)
+
 # print(coordoX[0], coordoY[0])
