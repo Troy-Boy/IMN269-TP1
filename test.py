550902b804db42fe91fe659997dedd3b4e222714
lana2914 <achille.lanctot-saumure@usherbrooke.ca>
2021-06-18 15:26:13 -0400

    ca perspective pas mal icitte

--- test.py
+++ test.py
@@ -19,22 +19,9 @@ coordoY = []
 coordo = [(3.17, 2.78, 1), (4.21, 2.12, 1), (2.14, 2.11, 1), (3.17, 1.81, 1),
           (3.18, 2.79, 1), (2.13, 2.12, 1), (4.21, 2.12, 1), (3.18, 1.81, 1)]
 VP = [(0.33, 1.0, 1), (0.6, 1.0, 1)]
-# vp1 = (0, 0, 0)
-# vp2 = (0, 0, 0)
-# horizon = [0, 0, 0]
 
 
 def update(event, x, y, flags, param):
-    # if event == cv2.EVENT_LBUTTONDOWN:
-    #     xy = "%d,%d" % (x, y)
-    #     coordoX.append(x)
-    #     coordoY.append(y)
-    #     cv2.circle(img, (x, y), 1, (0, 0, 255), thickness=-1)
-    #     cv2.putText(img, xy, (x, y), cv2.FONT_HERSHEY_PLAIN,
-    #                 1.0, (0, 0, 0), thickness=1)
-    #     cv2.imshow("fenetre image", img)
-    #     print(x,y)
-
     if (event == cv2.EVENT_LBUTTONDOWN):
         # xy = "%d,%d" % (x, y)
         # print(x, y)
@@ -46,64 +33,87 @@ def update(event, x, y, flags, param):
         #     cv2.imshow("fenetre image", img)
 
         # coordo.append((x/100, y/100, 1))
+
         if(len(coordo) == 8):
+            # calcul des points d'intersection
             vp1 = calculIntersection(0)
             vp2 = calculIntersection(4)
-            p1 = [int(vp1[0]), int(vp1[1])]
-            p2 = [int(vp2[0]), int(vp2[1])]
+            realVp1 = abs(vp1*100/vp1[2])
+            realVp2 = abs(vp2*100/vp2[2])
+            print("VPs:", vp1, vp2)
+            p1 = [int(realVp1[0]), int(realVp1[1])]
+            p2 = [int(realVp2[0]), int(realVp2[1])]
 
+            # affichage des points et horizon
             print(p1, p2)
             cv2.circle(img, p1, 2, (255, 0, 0), thickness=2)
             cv2.imshow("fenetre image", img)
             cv2.circle(img, p2, 2, (255, 0, 0), thickness=2)
             cv2.imshow("fenetre image", img)
+            cv2.line(img, [20, 99], [620, 99], (86, 137, 200), thickness=2)
+            cv2.line(img, p1, p2, (255, 0, 0), thickness=1)
 
+            cv2.imshow("fenetre image", img)
+
+            # Calcul de l'horizon
+            # horizon = np.cross([0.33, 1, 1], [6, 1, 1])
+            horizon = np.cross(vp1, vp2)
+
+            print("horizon:", horizon)
+            horizon[0] = 0
+            print("horizon:", horizon)
+
+            realHorizon = abs(horizon*100/horizon[2])
+            print("real horizon", realHorizon)
+
+            # calcul de H2
+            h2 = calculateH(horizon)
+            print("H:", h2)
+
+            # x = np.matmul(h2, vp2)
+
+            # print("X", x)
+            # h22 = np.linalg.inv(h2)
+            # y = h22.transpose()
+            # t = np.matmul(y, horizon)
+            # print("T", t)
+
+            # unwarp
+            newImg = unwarpImage(h2)
+            # cv2.imshow("res", newImg)
+            cv2.imwrite("res.jpg", newImg)
+            cv2.destroyAllWindows()
             print("fini")
+            exit()
 
 
-def warpPerspFuckingShit(img, H):
-    sizeX = len(img)
-    sizeY = len(img[0])
-    print(H)
-    maxX = 0
-    maxY = 0
-    minX = 999999999
-    minY = 999999999
-    for y in range(0, sizeY-1):
-        for x in range(0, sizeX-1):
-            posInit = np.array([x, y, 1])
-            posRes = posInit.dot(H)
-            resX = int(posRes[0])
-            resY = int(posRes[1])
-            # if(xRes!=0):
-            #     print("res x : ", xRes)
-            # if(xRes!=0):
-            #     print("res y : ", yRes)
-            if resX < minX:
-                minX = resX
-            if resX > maxX:
-                maxX = resX
-            if resY < minY:
-                minY = resY
-            if resY > maxY:
-                maxY = resY
-    newSizeX = maxX - minX
-    newsizeY = maxY - minY
-    imgRes = np.zeros((newSizeX+1, newsizeY+1, 3))
-    for y in range(0, sizeY-1):
-        for x in range(0, sizeX-1):
-            posInit = np.array([x, y, 1])
-            posRes = posInit.dot(H)
-            resX = int(posRes[0])
-            resY = int(posRes[1])
-            imgRes[resX-minX, resY-minY] = img[x, y]
-    imgFinal = cv2.resize(imgRes, (sizeX, sizeY), interpolation=cv2.INTER_AREA)
-    print("max X : ", maxX)
-    print("min X : ", minX)
-    print("max Y : ", maxY)
-    print("min Y : ", minY)
+def unwarpImage(h2):
+    newImg = np.zeros_like(img)
+    imH = len(img[0])
+    imW = len(img[1])
 
-    return imgFinal
+    # print(newImg, len(newImg), len(newImg[0]))
+    # print(len(img), len(img[370][630]))
+    for i in range(0, 630):
+        for j in range(0, 369):
+            pointToTransform = [i/100, j/100, 1]
+            transf = np.matmul(h2, pointToTransform)
+            # print(pointToTransform)
+            transf = abs(transf*100/transf[2])
+            # if(i % 20 == 0):
+            # print("transfo", transf)
+            newX = int(transf[0])
+            newY = int(transf[1])
+            # print("new x:", newX, "new y:", newY)
+            # print("i:", i, "j:", j)
+
+            if(newX < 631 and newY < 370):
+                t = img[j][i]
+                newImg[newY][newX] = t
+            # else:
+            #     print("out of bounds")
+
+    return newImg
 
 
 def calculateH(df):
@@ -114,27 +124,17 @@ def calculateH(df):
     return h2
 
 
-def printCoordo():
-    for i in range(0, 7):
-        cv2.circle(img, (coordoX[i], coordoY[i]), 1, (0, 0, 255), thickness=-1)
-        cv2.imshow("fenetre image", img)
-
-
 def calculIntersection(index):
     print(coordo)
     d1 = np.cross(coordo[index], coordo[index+1])
     d2 = np.cross(coordo[index+2], coordo[index+3])
     vp = np.cross(d1, d2)
-    vp = abs(vp*100/vp[2])
-
     print("VP:", vp)
     return vp
 
-    # d3 = np.cross(coordo[index], coordo[index+1])
-    # d4 = np.cross(coordo[index+2], coordo[index+])
-    # vp2 = np.cross(d3, d4)
-    # vp2 = abs(vp2*100/vp2[2])
-    # print("vp2:", vp2)
+
+# def horizon():
+#     d1 =
 
 
 cv2.namedWindow("fenetre image")
